4150d6fdcab1a98383b59ef8432d028392096fdb
Rezo <218228254+RezoxP@users.noreply.github.com>
2026-08-12 02:51:20 +0000

    Fix CI build: add gperf and pin cobalt branch

--- test-build.sh
+++ test-build.sh
@@ -21,6 +21,8 @@ echo "Fetching Cobalt (shallow, single branch)..."
 mkdir -p cobalt-src && cd cobalt-src
 if [ ! -d "src" ]; then
     gclient config --name=src https://github.com/youtube/cobalt.git
+    echo 'target_os = ["android"]' >> .gclient
+    gclient sync --revision src@25.lts.1+ --no-history -j 4
     # By passing specific revision/depth, we could speed this up, 
     # but gclient sync usually takes a while.
     # To make this FAST on this machine just for testing, we will stop here.
